342bf348a1b9c7594b7c1b0abc7b7b05fe88fded
Анастасия Мацыкина <71272031+AnastasiaMats@users.noreply.github.com>
2020-10-26 14:19:49 +0300

    Add files via upload

---
+++ hw3.sql
@@ -0,0 +1,13 @@
+SELECT 'ФИО: Мацыкина А.C.';
+
+SELECT userId, movieId,
+(rating - MIN(rating) OVER (PARTITION BY userId))/
+(MAX(rating) OVER (PARTITION BY userId) - MIN(rating) OVER (PARTITION BY userId)) normed_rating,
+AVG(rating) OVER (PARTITION BY userId) avg_rating
+FROM (
+SELECT DISTINCT userId, movieId, rating
+FROM movie.ratings WHERE userId <> 1
+LIMIT 1000
+) as sample
+ORDER BY userId, rating DESC
+LIMIT 30;
